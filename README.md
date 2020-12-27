bcfca6b675a3e3dd816b76a9d8d131ed2042f30c
llalon <71791017+llalon@users.noreply.github.com>
2020-12-27 12:40:48 -0500

    Uncoupled functions
    Moved functions to func.py. Uncoupled background and theme color.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Tested and designed for Linux Mint Mate 20.1 however it should work on any distr
 This is based on the default Linux Mint theme Mint-Y
 
 ```bash
-sudo apt install mint-themes mint-y-icons python3-colormath python3-matplotlib python3-gi jg wget gir1.2-geoclue-2.0 python3-astral
+sudo apt install mint-themes mint-y-icons python3-colormath python3-matplotlib python3-gi jg wget gir1.2-geoclue-2.0
 pip3 install suntime
 ```
 
@@ -25,7 +25,6 @@ python3 main.py
 Coming soon:
 * GUI
 * Support for other desktops
-* Support for dynamic darkmode based on time of day
 
 
 ## Contributing
--- src/settings.py
+++ src/settings.py
@@ -7,6 +7,6 @@ DARK_MODE = "dynamic"
 BASE_THEME = "Mint-Y"
 BASE_WM_THEME = "Mint-Y"
 BASE_THEME_COLORS = ["green", "aqua", "brown", "blue", "grey", "orange", "pink", "purple", "red", "teal"]
-WALLPAPER_DIR = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_PICTURES) + "/spotlight"
-WALLPAPER_COUNTRY = "CA"
-WALLPAPER_URL = "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&lc=en,en-US&ctry=" + WALLPAPER_COUNTRY
+BG_DIR = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_PICTURES) + "/spotlight"
+BG_COUNTRY = "CA"
+BG_URL = "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&lc=en,en-US&ctry=" + BG_COUNTRY
